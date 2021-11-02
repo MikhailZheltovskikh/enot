7b6158bbb3f643d9ae470675e7627b60eb95dde9
Mikhail_Zheltovskikh <misha14051994@mail.ru>
2021-11-02 16:56:20 +0500

    added page ptoduct.html

--- app/js/main.js
+++ app/js/main.js
@@ -3,3 +3,25 @@ $('.menu__btn').on('click', function () {
    $('.menu__btn').toggleClass('active');
 
 });
+
+$('.slider').slick({
+   arrows: false,
+   asNavFor: '.thumbs',
+   arrows: false,
+   slidesToShow: 1,
+   responsive: [
+      {
+        breakpoint: 520,
+        settings: {
+          dots: true,
+        }
+      }
+   ]
+});
+$('.thumbs').slick({
+   slidesToShow: 3,
+   arrows: false,
+   focusOnSelect: true,
+   asNavFor: '.slider',
+   vertical: true,
+});
